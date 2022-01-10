65333fb2b878873e2132fdaeb532e652d1d4b905
Kenny-54 <benoit.filippa@gmail.com>
2022-01-10 20:40:25 +0100

    Modif page test

--- docs/tutorial-basics/intro.md
+++ docs/tutorial-basics/intro.md
@@ -5,3 +5,5 @@ sidebar_position: 1
 # Nouvelle page !
 
 Ceci est un nouvelle page de test 😎
+
+Et voila une modif !
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,12 +24,12 @@ const config = {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/Kenny-54/my-docs/tree/main/',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/Kenny-54/my-docs/tree/main/docs/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
